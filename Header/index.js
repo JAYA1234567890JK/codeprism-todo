6c3eb37e4c5b667b36c086121e6328abd26db131
JAYA1234567890JK <101865581+JAYA1234567890JK@users.noreply.github.com>
2022-12-23 18:40:25 +0530

    Add files via upload

---
+++ Header/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='heading'>Todo-List</h1>
+    </div>
+  )
+}
+
+export default Header
